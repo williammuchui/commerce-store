db94cc77687a0659989d20e25e1130b5bcede0fc
william muchui <williammuchui584@gmail.com>
2024-08-01 11:08:19 +0300

    added payments, countries and tows API routes

--- server/server.ts
+++ server/server.ts
@@ -65,6 +65,8 @@ app.delete("/user/delete/:id", (req: Request, res: Response) => {
         res.status(200).json(result);
     });
 });
+
+//users addresses
 app.post("/user/addresses/new", (req: Request, res: Response) => {
     const { user_id, address_1_id, address_2_id, town_id, country_id } = req.body;
     const query = `insert into users_addresses (user_id, address_1, address_2, town_id, country_id) value(?, ?, ?, ?, ?)`;
@@ -124,6 +126,43 @@ app.patch("/user/address/default/update/:id", (req: Request, res: Response) => {
     const { address_id } = req.body;
     const query = `update default_address set address = ? where user_id = ?`;
     conn.query(query, [address_id, user_id], (err: Error, result: any) => {
+        if (err) throw err;
+        res.status(200).json(result);
+    });
+});
+//users payment methods
+app.post("/user/payments/new", (req: Request, res: Response) => {
+    const { user_id, account, payment_id } = req.body;
+    const query = `insert into users_payments (user_id, account, payment_id) values(?, ?, ?)`;
+    conn.query(query, [user_id, account, payment_id], (err: Error, result: any) => {
+        if (err) throw err;
+        res.status(200).json(result);
+    });
+});
+
+app.get("/user/payments/:id", (req: Request, res: Response) => {
+    const { id } = req.params;
+    const query = `select * from users_payments where user_id = ?`;
+    conn.query(query, [id], (err: Error, result: any) => {
+        if (err) throw err;
+        res.status(200).json(result);
+    });
+});
+
+app.patch("/user/payments/update/:id", (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { account, payment_id } = req.body;
+    const query = `update users_payments set account = ?, payment_id = ? where user_id = ?`;
+    conn.query(query, [account, payment_id, id], (err: Error, result: any) => {
+        if (err) throw err;
+        res.status(200).json(result);
+    });
+});
+
+app.delete("/user/payments/delete/:id", (req: Request, res: Response) => {
+    const { id } = req.params;
+    const query = `delete from users_payments where user_id = ?`;
+    conn.query(query, [id], (err: Error, result: any) => {
         if (err) throw err;
         res.status(200).json(result);
     });
@@ -202,6 +241,106 @@ app.patch("/address/update/:id", (req: Request, res: Response) => {
 
 })
 //Country APIs
+
+app.post("/country/new", (req: Request, res: Response) => {
+    const { name } = req.body;
+    const query = `insert into countries name values(?)`;
+    conn.query(query, [name], (err: Error, result: any) => {
+        if (err) throw err;
+        res.status(200).json(result);
+    });
+});
+
+app.get("/country/:id", (req: Request, res: Response) => {
+    const { id } = req.params;
+    const query = `select * from countries where id = ?`;
+    conn.query(query, [id], (err: Error, result: any) => {
+        if (err) throw err;
+        res.status(200).json(result);
+    });
+});
+
+app.get("/countries", (req: Request, res: Response) => {
+    const query = `select * from countries`;
+    conn.query(query, (err: Error, result: any) => {
+        if (err) throw err;
+        res.status(200).json(result);
+    });
+});
+
+//Towns APIs
+app.post("/town/new", (req: Request, res: Response) => {
+    const { name } = req.body;
+    const query = `insert into towns name values(?)`;
+    conn.query(query, [name], (err: Error, result: any) => {
+        if (err) throw err;
+        res.status(200).json(result);
+    });
+});
+
+app.get("/town/:id", (req: Request, res: Response) => {
+    const { id } = req.params;
+    const query = `select * from towns where id = ?`;
+    conn.query(query, [id], (err: Error, result: any) => {
+        if (err) throw err;
+        res.status(200).json(result);
+    });
+});
+
+app.get("/towns", (req: Request, res: Response) => {
+    const query = `select * from towns`;
+    conn.query(query, (err: Error, result: any) => {
+        if (err) throw err;
+        res.status(200).json(result);
+    });
+});
+
+//Payment Systems
+
+app.post("/payments/new", (req: Request, res: Response) => {
+    const { name } = req.body;
+    const query = `insert into payment_methods (name) values(?)`;
+    conn.query(query, [name], (err: Error, result: any) => {
+        if (err) throw err;
+        res.status(200).json(result);
+    });
+});
+
+app.get("/payments", (req: Request, res: Response) => {
+    const query = `select * from payment_methods`;
+    conn.query(query, (err: Error, result: any) => {
+        if (err) throw err;
+        res.status(200).json(result);
+    });
+});
+
+app.get("/payments/:id", (req: Request, res: Response) => {
+    const { id } = req.params;
+    const query = `select * from payment_methods where id = ?`;
+    conn.query(query, (err: Error, result: any) => {
+        if (err) throw err;
+        res.status(200).json(result);
+    });
+});
+
+app.delete("/payments/delete/:id", (req: Request, res: Response) => {
+    const { id } = req.params;
+    const query = `delete from payment_methods where id = ?`;
+    conn.query(query, (err: Error, result: any) => {
+        if (err) throw err;
+        res.status(200).json(result);
+    });
+});
+
+app.patch("/payments/update/:id", (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    const query = `update payment_methods set name = ? where id = ?`;
+    conn.query(query, [name, id], (err: Error, result: any) => {
+        if (err) throw err;
+        res.status(200).json(result);
+    });
+});
 
 //listen
 app.listen(3000, () => console.log("Server at port 3000!"));
